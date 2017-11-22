8056c7e7de120ea1a91772055bbbce9189900048
Remmy Clay <remmy.lauren@gmail.com>
2017-11-22 00:25:28 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,6 +15,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(//) ? true : false
+  phone.match(/\d{10}|(\d{3})\d{3}-\d{4}|\d{3} \d{3} \d{4}|(\d{3})\d{7}/) ? true : false
 #  2438894546", "(718)891-1313", "234 435 9978", "(800)4261134
 end
