888f44f0b515ed78fa73b49f0e8589e6c18b61e3
Remmy Clay <remmy.lauren@gmail.com>
2017-11-22 00:08:46 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/^[aeiouAEIOU]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z]\w+\W\z/) ? true : false
 end
 
 def valid_phone_number?(phone)
+  
 
+  2438894546", "(718)891-1313", "234 435 9978", "(800)4261134
 end
