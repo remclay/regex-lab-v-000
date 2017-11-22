ff5e63cd4fc981e5c21869ddbab731d003d34c98
Remmy Clay <remmy.lauren@gmail.com>
2017-11-22 00:50:50 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,5 +17,5 @@ end
 def valid_phone_number?(phone)
   #phone.match(/\d{10}|(\d{3})\d{3}-\d{4}|\d{3} \d{3} \d{4}|(\d{3})\d{7}|\d{7}/) ? true : false
 #  2438894546", "(718)891-1313", "234 435 9978", "(800)4261134
-  phone.match(/\(?\d{3}\)?[ -]?\d{3}[ -]?\d{4}\b/) ? true : false
+  phone.match(/(?\d{3}\)?[ -]?\d{3}[ -]?\d{4}\b/) ? true : false
 end
